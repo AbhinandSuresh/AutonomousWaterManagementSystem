6fd16b0dc4ec363e4cb6e960af4350dfca055ded
Vishnupvj <86926338+Vishnupvj@users.noreply.github.com>
2021-08-04 11:32:08 +0530

    Update LoRaReceiver_WMM.ino

--- LoRaReceiver_WMM.ino
+++ LoRaReceiver_WMM.ino
@@ -1,95 +1,66 @@
 #include <SPI.h>
 #include <LoRa.h>
-//define the pins used by the transceiver module
+
 #define ss D8
 #define rst D0
 #define dio0 D2
+#define motor LED_BUILTIN
 
+String device_id;
+String Lora_Ra02;
+int pos1, pos2;
 
 void setup() {
-  //initialize Serial Monitor
-  //pinMode(motor,OUTPUT);
-  pinMode(LED_BUILTIN,OUTPUT);
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println("LoRa Receiver Initialized!!!");
-  //setup LoRa transceiver module
+  pinMode(motor, OUTPUT);
+  /*Serial.begin(9600);
+    while (!Serial);
+    Serial.println(" ");
+    Serial.println("LoRa Receiver Callback");*/
   LoRa.setPins(ss, rst, dio0);
-  
-  //replace the LoRa.begin(---E-) argument with your location's frequency 
-  //433E6 for Asia
-  //866E6 for Europe/Africa
-  //915E6 for North America
-  if(!LoRa.begin(433E6)) {
-    Serial.println(".");
+  if (!LoRa.begin(433E6)) {
+    //    Serial.println("Starting LoRa failed!");
     while (1);
   }
 
-  Serial.println("LoRa Initializing OK!");
+  // put the radio into receive mode
+  LoRa.receive();
+  Serial.print("Receive Mode");
 }
 
-String getValue(String data, char separator, int index)
-{
-    int found = 0;
-    int strIndex[] = { 0, -1 };
-    int maxIndex = data.length() - 1;
-
-    for (int i = 0; i <= maxIndex && found <= index; i++) {
-        if (data.charAt(i) == separator || i == maxIndex) {
-            found++;
-            strIndex[0] = strIndex[1] + 1;
-            strIndex[1] = (i == maxIndex) ? i+1 : i;
-        }
-    }
-    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
-void loop() 
-{
-  String LoRaData; 
+void loop() {
   int packetSize = LoRa.parsePacket();
-  
-  if (packetSize) 
+  if (packetSize)
   {
-    // received a packet
-    Serial.print("\nReceived packet '");
-    // read packet
-    while (LoRa.available())
-    {
-      LoRaData = LoRa.readString();
-      Serial.print(LoRaData); 
-    }
+    String LoRaData = LoRa.readString();
 
-String DID = getValue(LoRaData, '>', 0);
-String threshold = getValue(LoRaData, '>', 1);
+    //    while (LoRa.available()) {
+    //    LoRa.read();
+    //}
 
-Serial.println("\nDevice ID:" + DID);
-Serial.print("\nThreshold:" + threshold);
-    
-    // print RSSI of packet
-Serial.print(" with RSSI ");
-Serial.println(LoRa.packetRssi());
+    pos1 = LoRaData.indexOf('>');
+    pos2 = LoRaData.indexOf('&');
 
-int LoRaval=threshold.toInt();
-Serial.println(LoRaval);
+    device_id = LoRaData.substring(0, pos1);
+    Lora_Ra02 = LoRaData.substring(pos1 + 1, pos2);
 
-    if(LoRaval < 25)
-      {
-        digitalWrite(LED_BUILTIN, HIGH);
-        //digitalWrite(motor, LOW);
-        /*for(i=0;i<120;i++)
-        /*for(i=0;i<120;i++)
-        {
-        LoRa.beginPacket();
-        LoRa.print("mon");
-        LoRa.endPacket();
-      }*/
-      }
-      if(LoRaval > 150)
-      {
-          digitalWrite(LED_BUILTIN, LOW);
-          //digitalWrite(motor, HIGH);
-        //for(i=0;i<120;i++)
-      }
-    
-   }
+    Serial.print(F("Device ID: "));
+    Serial.println(device_id);
+
+    Serial.print(F("Data Received from LoRa: "));
+    Serial.print(Lora_Ra02);
+    Serial.print(" centimeters;  with RSSI ");
+    Serial.println(LoRa.packetRssi());
+    Serial.println("");
+  }
+
+  //Compare received value to turn off or on the motor/LED
+  int loraval = Lora_Ra02.toInt();
+  if (loraval < 21)
+  {
+    digitalWrite(motor, HIGH);
+  }
+  else if (loraval > 130)
+  {
+    digitalWrite(motor, LOW);
+  }
 }
