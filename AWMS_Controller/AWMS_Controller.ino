267f8b28b7fce025d443f22d61a8dd55e996bc28
peterpathros <peetrooty31@gmail.com>
2021-10-16 21:56:32 +0530

    Created custom library for OLED display,added initialization fucntions to the main thread

--- AWMS_Controller/AWMS_Controller.ino
+++ AWMS_Controller/AWMS_Controller.ino
@@ -9,11 +9,20 @@
 */
 #include <SPI.h>                // include libraries
 #include <LoRa.h>
+#include <Wire.h>
+#include "WMMOled.h"
+#include "SSD1306Wire.h"
 #define LORA_FREQUENCY 433E6    // Set LoRa frequency
 #define SERIAL_BAUD 9600        // Set serial baud rate for debugging
 #define CS_PIN 15               // LoRa radio chip select
 #define RST_PIN 16              // LoRa radio reset
 #define IRQ_PIN 4               // change for your board; must be a hardware interrupt pin
+#define SDA_PIN 10              //OLED I2c Data pin 
+#define SCL_PIN 9               //OLED I2c Clock pin 
+#define ADDRESS 0x3c            //address of 126X64 OLED
+/Declaring Object of SSD1306Wire :@address ,@SDA_PIN,@SCK_PIN
+SSD1306Wire display(ADDRESS,SDA_PIN,SCL_PIN); 
+
 /*
  * Define DEBUG for Debug Mode
  * DEBUG_LEVEL 0,1,2 can be used for various debugging levels
@@ -42,6 +51,8 @@ void setup()
   //Initialize Serial communication for debugging.
   Serial.begin(SERIAL_BAUD);
   debug_Println(1,"\nSerial Init Success...");
+  //Initialize I2C Communication pin for OLED display.
+  Wire.begin(SDA_PIN,SCL_PIN);
   //Initialize LoRa for radio communication
   LoRa.setPins(CS_PIN, RST_PIN, IRQ_PIN);
   if (!LoRa.begin(LORA_FREQUENCY)) 
@@ -55,7 +66,7 @@ void setup()
   //gpioInit();
 
   //Initialize OLED
-  //oledInit(sda,scl);
+  oledInit(display);
 
   //initialize WiFi
   //create_access_point.
@@ -71,7 +82,7 @@ void setup()
   //aesInit();
   
   //initialize file system
-   if (SPIFFS.begin()) 
+   *if (SPIFFS.begin()) 
    {
     debug_Println(1,"\nSPIFFS Active");
   } 
@@ -83,7 +94,7 @@ void setup()
 
   //set button gpio as input hardware interupt
   //ISR for button interupt(set number as per secs)
-}
+
 
 void loop() 
 {
