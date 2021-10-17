2bc0b1119aae1bdfe5b122b66f8165b7fe4145fc
peterpathros <86644926+peterpathros@users.noreply.github.com>
2021-10-18 00:59:33 +0530

    Update WMMOled.ino

--- examples/WMMOled/WMMOled.ino
+++ examples/WMMOled/WMMOled.ino
@@ -5,7 +5,7 @@
 #define SDA 10             //Data pi of In
 #define SCL 9              //Clock pin
 #define BAUD 9600          //Baud rate
-#define ADDRESS 0x3c   //Address  for 128x96 OLED 
+#define ADDRESS 0x3c       //Address  for 128x96 OLED 
 
 SSD1306Wire display(ADDRESS, SDA, SCL);
 
@@ -13,8 +13,7 @@ void setup() {
   Serial.begin(BAUD);
   Wire.begin(SDA, SCL); //defining the custom pins for I2C interface
   
-  oledInit(display);
-  //intializing the dispaly:@Object of SSD1306Wire
+  oledInit(display);    //intializing the dispaly:@Object of SSD1306Wire
 }
 
 void loop() {
