41c6e0007968e5da8e4346ca8e89cf70296576b5
sujith nair <sujith.nair711@gmail.com>
2021-10-19 09:35:04 +0530

    added debug mode and removed Serial.Println()

--- Headers/SPIFFSFileHandler/SPIFFSFileHandler.cpp
+++ Headers/SPIFFSFileHandler/SPIFFSFileHandler.cpp
@@ -7,6 +7,29 @@
 #include "FS.h"
 #include "SPIFFSFileHandler.h"
 
+*
+ * Define DEBUG for Debug Mode
+ * DEBUG_LEVEL 0,1,2 can be used for various debugging levels
+ * 2: Verbose mode
+ * 1: Debug mode
+ * 0: End user mode
+*/
+#define DEBUG 
+#define DEBUG_LEVEL 1
+#ifdef DEBUG
+#define debug_Println(level,msg) { \
+        if(DEBUG_LEVEL >= level) \
+        debug_Println(1,msg); \
+        }
+#define debug_Print(level,msg) { \
+        if(DEBUG_LEVEL >= level) \
+        debug_Println(1,msg); \
+        }
+#else
+#define debug_Println(x)
+#define debug_Print(x)
+#endif
+
 /*
    This function reads data from a file. the Function accepts filename and String variable to store the data
    Parameters
@@ -19,7 +42,7 @@ void readFile(char * filename, String data)
   File MyFile = SPIFFS.open(filename, "r");// open the file in read mode
   if (!MyFile)
   {
-    Serial.println(F("File not found"));
+    debug_Println(1,"File not found"));
   }
   while (MyFile.available()) // Read line by line from the file
   {
@@ -41,13 +64,13 @@ void writeFile(char * filename, String data)
   File f = SPIFFS.open(filename, "a");// create or open the file in append mode.
   if (!f)
   {
-    Serial.println("File not found");
+    debug_Println(1,"File not found");
   }
   else
   {
     f.printf("%s\n", data); //Write the data in the file
-    Serial.print("data saved in: ");
-    Serial.println(filename);
+    debug_Println(1,"data saved in: ");
+    debug_Println(1,filename);
   }
 
   f.close();
@@ -61,4 +84,5 @@ void writeFile(char * filename, String data)
 void removeFile(char * filename) 
 {
   SPIFFS.remove(filename);
+  debug_Println(1,"File is removed");
 }
